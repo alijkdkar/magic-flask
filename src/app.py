dcb28f253c3a694e53dd69c85f1942be52074815
leo <alijkdkar@yahoo.com>
2024-02-02 01:57:43 +0330

    first initial and run success

---
+++ src/app.py
@@ -0,0 +1,15 @@
+import os
+
+
+print("in it")
+# App Initialization
+from . import create_app # from __init__ file
+app = create_app(os.getenv("CONFIG_MODE"))
+
+# Hello World!
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
